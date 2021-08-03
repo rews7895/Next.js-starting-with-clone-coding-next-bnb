d1df8fab46fbc119aac4c17d1e5a1d2f978ce9fa
Junseok <yalvz@naver.com>
2021-08-03 22:35:53 +0900

    1. 프로젝트 설정 마무리
    넥스트 프로젝트의 기본 설정 마무리
    1. eslint
    2. babel
    3. 기본 next.js 구조
    4. styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-tabs */
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  font-size: 21px;
+  color: grey;
+`;
+
+const index: React.FC = () => {
+	return <Container>Hello world</Container>;
+};
+
+export default index;
